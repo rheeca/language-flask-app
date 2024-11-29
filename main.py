2cbd0b2e84db76603c11178be4e74a16f7c2904a
Rheeca Guion <144939073+rheeca@users.noreply.github.com>
2024-11-29 11:08:21 -0500

    html layouts, list words from image

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import base64
 import cv2
 import io
 import numpy as np
+import os
 
 from flask import Flask, render_template, request
 from google.cloud import storage, vision
 from PIL import Image
 from typing import Sequence
 
-BUCKET_NAME = ""
+BUCKET_NAME = os.getenv('BUCKET_NAME')
 
 
 app = Flask(__name__)
@@ -37,13 +38,13 @@ def print_text(response: vision.AnnotateImageResponse):
             sep=" | ",
         )
 
-def draw_bounding_box(image_bytes, result):
+def draw_bounding_box(image_bytes, objects):
     # Draw bounding box on image
     nparr = np.frombuffer(image_bytes, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    for annotation in result.text_annotations:
-        vertices = annotation.bounding_poly.vertices
+    for o in objects:
+        vertices = o.bounding_poly.vertices
         cv2.rectangle(image, (vertices[0].x, vertices[0].y), (vertices[2].x, vertices[2].y), (0, 255, 0), 2) 
 
     _, encoded_image = cv2.imencode('.jpg', image)
@@ -62,8 +63,8 @@ def show_image():
         image_bytes = f.read()
 
         # Connect to Cloud Storage
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(BUCKET_NAME)
+        # storage_client = storage.Client()
+        # bucket = storage_client.bucket(BUCKET_NAME)
 
         # Download image from Cloud Storage
         # blob = bucket.blob("test3.jpg")
@@ -71,11 +72,16 @@ def show_image():
 
         # Vision processing
         response = analyze_image_from_local(image_bytes=image_bytes, feature_types=[vision.Feature.Type.TEXT_DETECTION])
-        result_bytes = draw_bounding_box(image_bytes=image_bytes, result=response)
+        result_bytes = draw_bounding_box(image_bytes=image_bytes, objects=response.text_annotations)
+        
+        words = {}
+        annotations = response.text_annotations[1:]
+        for v in annotations:
+            words[v.description] = {'translation': None}
         
         # Upload result to Cloud Storage
-        blob = bucket.blob(f.filename)
-        blob.upload_from_string(result_bytes, content_type='image/jpeg')
+        # blob = bucket.blob(f.filename)
+        # blob.upload_from_string(result_bytes, content_type='image/jpeg')
 
         # Save image as in-memory file
         img = Image.open(io.BytesIO(result_bytes))
@@ -83,7 +89,13 @@ def show_image():
         img.save(buffer, 'JPEG')
         encoded_img_data = base64.b64encode(buffer.getvalue())
 
-    return render_template('image.html', image=encoded_img_data.decode('utf-8'))
+    return render_template('image.html', 
+                           image=encoded_img_data.decode('utf-8'), 
+                           words=words)
+
+@app.route("/study", methods=['GET', 'POST'])
+def study():
+    return render_template('study.html', title='Study')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
